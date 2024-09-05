33090733de2cef0d3794fade39607d6f27cc7b34
Jeremy Dyer <jdye64@gmail.com>
2024-09-05 10:38:00 -0400

    Setup redirect to nv-ingest

--- tests/nv_ingest_client/cli/test_nv_ingest_cli.py
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES.
-# All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-# TODO(Devin): Just for coverage at the moment
-import nv_ingest_client.nv_ingest_cli as nv_ingest_cli  # noqa: F401
